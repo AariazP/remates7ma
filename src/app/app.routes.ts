181f552cdd10a4bedab7e426080d9d9bdfaf9dd5
Alejandro Arias <aariaz@Alejandros-Laptop.local>
2024-02-14 17:01:50 -0500

    first_commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '**',
+        component: LoginComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    }
+];
